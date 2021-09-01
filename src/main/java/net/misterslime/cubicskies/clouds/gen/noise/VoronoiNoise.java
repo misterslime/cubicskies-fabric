e6cbe136b83083e8f622ccc194099aa711b8de00
misterslime <30942693+misterslime@users.noreply.github.com>
2021-09-01 12:22:05 -0600

    Get cell distance + 3D voronoi noise

--- src/main/java/net/misterslime/cubicskies/clouds/gen/noise/VoronoiNoise.java
+++ src/main/java/net/misterslime/cubicskies/clouds/gen/noise/VoronoiNoise.java
@@ -9,10 +9,25 @@ public class VoronoiNoise {
         fastNoiseLite.SetFrequency(frequency);
         fastNoiseLite.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
         fastNoiseLite.SetCellularDistanceFunction(FastNoiseLite.CellularDistanceFunction.Euclidean);
-        fastNoiseLite.SetCellularReturnType(FastNoiseLite.CellularReturnType.CellValue);
     }
 
     public float sample(float x, float z) {
+        fastNoiseLite.SetCellularReturnType(FastNoiseLite.CellularReturnType.CellValue);
         return fastNoiseLite.GetNoise(x, z);
+    }
+
+    public float sample(float x, float y, float z) {
+        fastNoiseLite.SetCellularReturnType(FastNoiseLite.CellularReturnType.CellValue);
+        return fastNoiseLite.GetNoise(x, y, z);
+    }
+
+    public float distance(float x, float z) {
+        fastNoiseLite.SetCellularReturnType(FastNoiseLite.CellularReturnType.Distance);
+        return fastNoiseLite.GetNoise(x, z);
+    }
+
+    public float distance(float x, float y, float z) {
+        fastNoiseLite.SetCellularReturnType(FastNoiseLite.CellularReturnType.Distance);
+        return fastNoiseLite.GetNoise(x, y, z);
     }
 }
